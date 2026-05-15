cdacef56ab66fb0661e8a530864c2d660a277af1
unknown <ayeshanoman490@gmail.com>
2026-05-16 00:18:16 +0500

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,18 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    'nativewind/babel',
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        alias: {
-          '@': './',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@': './',
+          },
         },
-      },
+      ],
+      'react-native-reanimated/plugin',  // must be last
     ],
-  ],
+  };
 };
